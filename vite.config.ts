5c973f0cfb6f6f4c3e8f870b35485fbd6375aa6c
Henr. Ferr <rickferr.dev@gmail.com>
2026-03-24 23:14:15 -0300

    feat: initialize react project with vite, ts, tailwind
    Initializes the frontend development environment.
    Integrates Vite, React with TypeScript, and Tailwind CSS.
    Configures ESLint for code quality and Bun for package management.
    Sets up an API proxy for development.

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+    server: {
+        proxy: {
+            "/api": "https://sketch-service-email.onrender.com"
+        }
+    },
+    plugins: [
+        tailwindcss(),
+        react(),
+        babel({ presets: [reactCompilerPreset()] }),
+    ],
+})
